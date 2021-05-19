4fb601fe0f6b2415c449b25763401b26689deea4
Teraiman <toshi.guitar.no1.9995@gmail.com>
2021-05-19 10:07:43 +0000

    3章:meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b8b1ca118cf8d8e92921fb9281c67149688f44650add0e30e1fa26516010579c32281bee46188d3a501c248a25ac57fe04abc14cce50cfde13baebc2a3a9ec3c'
